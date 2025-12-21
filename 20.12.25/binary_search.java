cf8ea403ed0d953db40e2947db3dd4d21cdeb539
Srikumaran-P <srikumaranp33@gmail.com>
2025-12-21 21:45:01 +0530

    Create binary_search.java

---
+++ 20.12.25/binary_search.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    TreeNode prev=null;
+    TreeNode first=null,last=null;
+    public void inorder(TreeNode root){
+        if(root!=null){
+            inorder(root.left);
+            if(prev!=null&&(root.val<prev.val)){
+                if(first==null){
+                    first=prev;
+                    last=root;
+                }
+                else
+                    last=root;
+            }
+            prev=root;
+            inorder(root.right);
+        } 
+    }
+    public void swap(TreeNode first,TreeNode last){
+        int temp=first.val;
+        first.val=last.val;
+        last.val=temp;
+    }
+    public void recoverTree(TreeNode root) {
+           inorder(root);
+           if(first!=null && last!=null)
+                swap(first,last);
+    }
+}
