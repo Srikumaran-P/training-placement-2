edc37f7535700e1a8019d4db2807580a835bd30a
Srikumaran-P <srikumaranp33@gmail.com>
2025-12-21 20:54:41 +0530

    Create search_insert_position.java

---
+++ 19.12.25/search_insert_position.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+       int start = 0;
+       int end = nums.length-1;
+       int ans = 0;
+       while(start <= end){
+        int mid = start + (end - start)/2;
+        if(nums[mid] == target) return mid;
+        else if(nums[mid] > target){
+            end = mid-1;
+        }else{
+            start = mid+1;
+            ans = mid+1;
+        }
+       }
+       return ans;
+    }
+}
