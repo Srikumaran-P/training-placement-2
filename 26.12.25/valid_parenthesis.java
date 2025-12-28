894bb4ad2c3079112757de843acef28ab40afecc
Srikumaran-P <srikumaranp33@gmail.com>
2025-12-28 18:16:34 +0530

    Create valid_parenthesis.java

---
+++ 26.12.25/valid_parenthesis.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length() %2 != 0) return false;
+        Stack <Character> st= new Stack<>();
+        for(int i=0;i< s.length();i++){
+            char ch= s.charAt(i);
+            if(ch== '(' || ch == '[' || ch == '{'){
+                st.push(ch);
+            }
+            else{
+                if(st.isEmpty()) return false;
+                else if(ch ==')' && st.peek()=='('){
+                    st.pop();
+                }
+                else if(ch =='}' && st.peek()=='{'){
+                    st.pop();
+                }
+                else if(ch== ']' && st.peek()=='['){
+                    st.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+    }
+}
