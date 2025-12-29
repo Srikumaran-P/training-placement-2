e62e641002cca5e6e8e1688f4f803e849d75e261
Srikumaran-P <srikumaranp33@gmail.com>
2025-12-29 15:57:19 +0530

    Create remove_duplicates.py

---
+++ 27.12.25/remove_duplicates.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+
+        k = 1  # next write position for a new unique value
+        for i in range(1, len(nums)):
+            if nums[i] != nums[k - 1]:
+                nums[k] = nums[i]
+                k += 1
+        return k
