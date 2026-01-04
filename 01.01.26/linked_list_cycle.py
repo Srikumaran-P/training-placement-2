ec79110fdfcac5dbaa33b9c828d61918ec61eee8
Srikumaran-P <srikumaranp33@gmail.com>
2026-01-04 08:22:51 +0530

    Create linked_list_cycle.py

---
+++ 01.01.26/linked_list_cycle.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def hasCycle(self, head):
+        visited = set()
+        current = head
+
+        while current:
+            if current in visited:
+                return True
+
+            visited.add(current)
+            current = current.next
+
+        return False
