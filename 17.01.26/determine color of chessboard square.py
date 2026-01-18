408081735ac4b4cd8a8b9188d1e44281f4659004
Srikumaran-P <srikumaranp33@gmail.com>
2026-01-18 23:35:17 +0530

    Create determine color of chessboard square.py

---
+++ 17.01.26/determine color of chessboard square.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def squareIsWhite(self, coordinates):
+        """
+        :type coordinates: str
+        :rtype: bool
+        """
+        if (ord(coordinates[0].lower()) - ord('a') + 1) % 2 == int(coordinates[1]) % 2:
+            return False
+        return True
